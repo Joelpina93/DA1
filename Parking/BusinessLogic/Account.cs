4ce47103389b7f0dd268ec69fcf624b2f9e723bf
Joelpina93 <54318454+Joelpina93@users.noreply.github.com>
2020-04-24 12:19:18 -0300

    Obligatorio Final
    Obligatorio Diseño de aplicaciones 1

---
+++ Parking/BusinessLogic/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Account
+    {
+        [ForeignKey("Purchase")]
+        public int AccountId { get; set; }
+        public string AccountCellPhoneNumber { get; set; }
+        public int AccountBalance { get; set; }
+
+        public virtual Purchase Purchase { get; set; }
+
+        public Account(string mobileNumber, int balance = 0)
+        {
+            this.AccountCellPhoneNumber = mobileNumber;
+            this.AccountBalance = balance;
+        }
+
+        public bool AccountBalanceIsPositive()
+        {
+            return this.AccountBalance >= 0;
+        }
+
+        public void AddMoneyToBalance(string moneyToAdd)
+        {
+            this.AccountBalance += Convert.ToInt32(moneyToAdd);
+        }
+
+
+        public bool SubstractMoneyFromBalance(int moneyToSustract)
+        {
+            bool success = false;
+            if(this.AccountBalance - moneyToSustract >= 0)
+            {
+                this.AccountBalance -= moneyToSustract;
+                success = true;
+            }
+            return success;
+        }
+
+    }
+}
